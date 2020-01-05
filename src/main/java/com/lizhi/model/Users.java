2270f34663019ffc7b81e6799b75fa50cbcdff0b
lizhi <949168418@qq.com>
2020-01-05 20:29:20 +0800

    springsecurity commit

---
+++ src/main/java/com/lizhi/model/Users.java
@@ -0,0 +1,53 @@
+package com.lizhi.model;
+
+public class Users {
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    private Byte enable;
+
+    private String roles;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username == null ? null : username.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public Byte getEnable() {
+        return enable;
+    }
+
+    public void setEnable(Byte enable) {
+        this.enable = enable;
+    }
+
+    public String getRoles() {
+        return roles;
+    }
+
+    public void setRoles(String roles) {
+        this.roles = roles == null ? null : roles.trim();
+    }
+}
