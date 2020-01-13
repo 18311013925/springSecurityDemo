4de2b41b64a781a1a6ee0e941c502ad42820f530
lizhi <741852@qq.com>
2020-01-13 18:58:32 +0800

    1、redis 配置修改 2、redisSession 集群会话管理失败，以后在研究吧

--- src/main/java/com/lizhi/SpringSecurityDemoApplication.java
+++ src/main/java/com/lizhi/SpringSecurityDemoApplication.java
@@ -3,17 +3,12 @@ package com.lizhi;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-
-import javax.sql.DataSource;
-import java.sql.SQLException;
 
 @SpringBootApplication
 @MapperScan("com.lizhi.mapper")
-public class SpringDemoApplication {
+public class SpringSecurityDemoApplication {
     public static void main(String[] args) {
-        SpringApplication.run(SpringDemoApplication.class);
+        SpringApplication.run(SpringSecurityDemoApplication.class);
     }
 
 }
--- src/main/java/com/lizhi/config/redisConfig/RedisClusterConfig.java
+++ src/main/java/com/lizhi/config/redisConfig/RedisClusterConfig.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
@@ -12,12 +14,10 @@ import org.springframework.cache.interceptor.CacheErrorHandler;
 import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.cache.RedisCacheWriter;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -26,9 +26,7 @@ import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-import javax.annotation.Resource;
 import java.lang.reflect.Method;
-import java.net.UnknownHostException;
 import java.time.Duration;
 import java.util.HashSet;
 import java.util.Set;
@@ -37,15 +35,15 @@ import java.util.Set;
 /**
  * @author: lizhi
  * @Date: 2019/11/21 18:50
- * @Description:  使用 jedisCluster redisTemplate stringRedisTemplate 均可以，但是数据不共享，不和一混合使用
+ * @Description: 使用 jedisCluster redisTemplate stringRedisTemplate 均可以，但是数据不共享，不和一混合使用
  */
 @Configuration
-@EnableCaching
+@AutoConfigureAfter(RedisAutoConfiguration.class)
+//@EnableCaching
 public class RedisClusterConfig extends CachingConfigurerSupport {
 
-    @Autowired
-    private LettuceConnectionFactory lettuceConnectionFactory;
-
+    /*@Autowired
+    private LettuceConnectionFactory connectionFactory;
 
     @Override
     public KeyGenerator keyGenerator() {
@@ -71,7 +69,7 @@ public class RedisClusterConfig extends CachingConfigurerSupport {
                 .entryTtl(Duration.ofSeconds(30))
                 .serializeValuesWith(serializationPair);
         return RedisCacheManager
-                .builder(RedisCacheWriter.nonLockingRedisCacheWriter(lettuceConnectionFactory))
+                .builder(RedisCacheWriter.nonLockingRedisCacheWriter(connectionFactory))
                 .cacheDefaults(redisCacheConfiguration).build();
     }
 
@@ -89,7 +87,7 @@ public class RedisClusterConfig extends CachingConfigurerSupport {
     @Override
     public CacheManager cacheManager() {
         RedisCacheManager.RedisCacheManagerBuilder builder = RedisCacheManager.RedisCacheManagerBuilder
-                .fromConnectionFactory(lettuceConnectionFactory);
+                .fromConnectionFactory(connectionFactory);
         @SuppressWarnings("serial")
         Set<String> cacheNames = new HashSet<String>() {
             {
@@ -98,24 +96,24 @@ public class RedisClusterConfig extends CachingConfigurerSupport {
         };
         builder.initialCacheNames(cacheNames);
         return builder.build();
-    }
+    }*/
 
     /**
      * 设置数据存入redis 的序列化方式
-     *</br>redisTemplate序列化默认使用的jdkSerializeable,存储二进制字节码,导致key会出现乱码，所以自定义
-     *序列化类
+     * </br>redisTemplate序列化默认使用的jdkSerializeable,存储二进制字节码,导致key会出现乱码，所以自定义
+     * 序列化类
      * 以下两种redisTemplate自由根据场景选择
      *
      * @paramredisConnectionFactory
      */
     @Bean
-    public RedisTemplate<String,Object> redisTemplate(){
-        RedisTemplate<String,Object> redisTemplate = new RedisTemplate<String, Object>();
-        redisTemplate.setConnectionFactory(lettuceConnectionFactory);
+    public RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory LettuceConnectionFactory ) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<String, Object>();
+        redisTemplate.setConnectionFactory(LettuceConnectionFactory);
 
-        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer =new Jackson2JsonRedisSerializer(Object.class);
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
 
-        ObjectMapper objectMapper =new ObjectMapper();
+        ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
         objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
         //使用Jackson2JsonRedisSerializer来序列化和反序列化redis的value值（默认使用JDK的序列化方式）
@@ -131,12 +129,11 @@ public class RedisClusterConfig extends CachingConfigurerSupport {
     }
 
     @Bean
-    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
+    public StringRedisTemplate stringRedisTemplate(LettuceConnectionFactory LettuceConnectionFactory) {
         StringRedisTemplate stringRedisTemplate = new StringRedisTemplate();
-        stringRedisTemplate.setConnectionFactory(redisConnectionFactory);
+        stringRedisTemplate.setConnectionFactory(LettuceConnectionFactory);
         return stringRedisTemplate;
     }
-
 
 
 }
